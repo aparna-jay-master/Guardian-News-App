ea7ff3e5af1f2fcba3d4b5e7091630b915bed0f8
Aparna <aparnajay.jay@gmail.com>
2017-07-08 21:44:47 -0700

    Update MainActivity to connect to internet, pull data, and parse it properly

--- app/src/main/java/com/example/android/guardiannewsapp/MainActivity.java
+++ app/src/main/java/com/example/android/guardiannewsapp/MainActivity.java
@@ -1,15 +1,171 @@
 package com.example.android.guardiannewsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<News>> {
+
+    /**
+     * Constant value for the book loader ID.
+     */
+    private static final int NEWS_LOADER_ID = 1;
+    /**
+     * URL for book data from the Google API
+     */
+    private String GUARDIAN_API =
+            "http://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test";
+    /**
+     * Adapter for the list of books
+     */
+    private NewsAdapter mAdapter;
+
+    /**
+     * TextView that is displayed when the list is empty
+     */
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //url setup happens here
+        //TODO implement url part
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        //find edit text
+        final EditText editTextView = (EditText) findViewById(R.id.search_text_view);
+
+        //Click listener
+        editTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                //Find search text and create variable
+                String textQuery = editTextView.getText().toString();
+                String adjustedQuery = textQuery.replace(" ", "+");
+                //TODO update this link
+                GUARDIAN_API = "http://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test" + adjustedQuery;
+
+                if (checkConnectivity()) {
+                    //configure loader manager
+                    LoaderManager loaderManager = getLoaderManager();
+                    loaderManager.restartLoader(1, null, MainActivity.this);
+                } else {
+                    mEmptyStateTextView.setText(R.string.no_internet_connection);
+                }
+                return false;
+            }
+        });
+
+        // Create a new adapter that takes an empty list of articles as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsListView.setAdapter(mAdapter);
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        return new NewsLoader(this, GUARDIAN_API);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> newses) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No articles found."
+        mEmptyStateTextView.setText(R.string.no_news);
+
+        // Clear the adapter of previous book data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link News}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (newses != null && !newses.isEmpty()) {
+            mAdapter.addAll(newses);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
+    }
+
+    public boolean checkConnectivity() {
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+            return true;
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator and list so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            View list = findViewById((R.id.list));
+            list.setVisibility(View.GONE);
+            //make list visible
+            mEmptyStateTextView.setVisibility(View.VISIBLE);
+            return false;
+        }
     }
 }
